24cf6da12083cf63280d360e0122dbb1792afa0b
xuyife <18916947008@163.com>
2024-09-04 17:59:22 +0800

    初始提交

---
+++ src/main/java/cool/js/xuyifei/UltimateHardeningClient.java
@@ -0,0 +1,10 @@
+package cool.js.xuyifei;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimateHardeningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
